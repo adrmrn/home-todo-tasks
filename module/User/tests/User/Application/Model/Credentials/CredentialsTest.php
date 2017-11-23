068aa46d7eac583fd4d6efca49fa28844fc4341e
Adrian <adrianmoron@o2.pl>
2017-11-24 00:28:45 +0100

    Credentials - unit tests

---
+++ module/User/tests/User/Application/Model/Credentials/CredentialsTest.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: adrian
+ * Date: 24.11.17
+ * Time: 00:25
+ */
+
+namespace User\Application\Model\Credentials;
+
+use PHPUnit\Framework\TestCase;
+use Shared\Application\ValueObject\Email;
+use User\Application\Utility\PasswordHasher;
+
+class CredentialsTest extends TestCase
+{
+    public function testCreateCredentials()
+    {
+        $email = new Email('test@gmail.com');
+        $password = PasswordHasher::hash('Qwerty123');
+
+        $credentials = new Credentials($email, $password);
+
+        $this->assertInstanceOf(Email::class, $credentials->email());
+        $this->assertInstanceOf(HashedPassword::class, $credentials->hashedPassword());
+        $this->assertEquals('test@gmail.com', $credentials->email()->toString());
+    }
+}
